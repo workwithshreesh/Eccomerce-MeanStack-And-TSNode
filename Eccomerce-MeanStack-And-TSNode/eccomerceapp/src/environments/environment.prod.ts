4ec7c61afdad0054de91a1f21f9bc1b81a07ee30
workwithshreesh <shreesht366@gmail.com>
2025-04-08 19:06:10 +0530

    deleted eccomerce angular 10 project folder unwanted

--- Eccomerce-MeanStack-And-TSNode/eccomerceapp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: "https://eccomercebackend-gwim.onrender.com",
-  product: "https://eccomercebackend-gwim.onrender.com/api/product",
-  category: "https://eccomercebackend-gwim.onrender.com/api/category",
-};
